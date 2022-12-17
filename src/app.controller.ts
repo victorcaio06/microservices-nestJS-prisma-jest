97fc361b9634bce253ed34058a50ccd3785858cf
Victor Caio <kaioaraujo426@gmail.com>
2022-12-17 04:41:24 -0300

    FEAT(Notifications): create notification endpoint incomplete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { randomUUID } from 'node:crypto';
+import { Notification } from '@prisma/client';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
   constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
   listAll(): Promise<any> {
     return this.prismaService.notification.findMany();
+  }
+
+  @Post('create')
+  async create(): Promise<Notification> {
+    return await this.prismaService.notification.create({
+      data: {
+        content: 'You have a new friendship request',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
