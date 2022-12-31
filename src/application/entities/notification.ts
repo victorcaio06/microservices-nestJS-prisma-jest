f9f3436fcc15813b2d131c2b7c36678d3b9b1757
Victor Caio <kaioaraujo426@gmail.com>
2022-12-31 13:19:16 -0300

    FEAT: added createdAt and Replace for createAt

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -15,8 +15,8 @@ export class Notification {
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>, id?: string) {
+    this._id = id ?? randomUUID();
     this.props = { ...props, createdAt: props.createdAt ?? new Date() };
   }
 
@@ -50,6 +50,10 @@ export class Notification {
 
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
   }
 
   public cancelNotification() {
